9c931c39dbeefab26a6b8e3c9c7b8266070e3db0
wei yu <104413257+firepiratex@users.noreply.github.com>
2024-11-05 15:08:50 +0800

    password complexity

--- HMS/src/handlers/LoginHandler.java
+++ HMS/src/handlers/LoginHandler.java
@@ -56,6 +56,7 @@ public class LoginHandler {
     }
 
     private void initialLogin(User user, Scanner scanner) {
+    	boolean length = false, upperCharacter = false, specialCharacter = false, number = false;
         System.out.println("This is your first time logging in so you need "
                 + "to change your password");
         while (true) {
@@ -68,6 +69,44 @@ public class LoginHandler {
                 if (confirmPassword.equals("password")) {
                     System.out.println("Cannot be the default password.");
                     continue;
+                }
+                if (confirmPassword.length() >= 8) {
+                	length = true;
+                }
+                for(int i = 0; i < confirmPassword.length(); i++) {
+                	if (confirmPassword.charAt(i) >= 65 && confirmPassword.charAt(i) <= 90) {
+                		upperCharacter = true;
+                	}
+                	if (confirmPassword.charAt(i) >= 48 && confirmPassword.charAt(i) <= 57) {
+                		number = true;
+                	}
+                	if ((confirmPassword.charAt(i) >= 33 && confirmPassword.charAt(i) <= 42) || confirmPassword.charAt(i) == 64) {
+                		specialCharacter = true;
+                	}
+                }
+                if (length == false || upperCharacter == false || specialCharacter == false || number == false) {
+                	System.out.println("The password does not meet the requirement.");
+                	if (length == false) {
+                		System.out.println("[x] More than 8 characters");
+                	} else {
+                		System.out.println("[/] More than 8 characters");
+                	}
+                	if (upperCharacter == false) {
+                		System.out.println("[x] At least one upper character");
+                	} else {
+                		System.out.println("[/] At least one upper character");
+                	}
+                	if (specialCharacter == false) {
+                		System.out.println("[x] At least one special character (!, \", #, $, %, &, ', (, ), *, @)");
+                	} else {
+                		System.out.println("[/] At least one special character (!, \", #, $, %, &, ', (, ), *, @)");
+                	}
+                	if (number == false) {
+                		System.out.println("[x] At least one number");
+                	} else {
+                		System.out.println("[/] At least one number");
+                	}
+                	continue;
                 }
                 System.out.println("You have changed your password successfully!");
                 user.setPassword(PasswordHash.hash(confirmPassword)); // Store the hashed password
